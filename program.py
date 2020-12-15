f4da4552dcc47f0d5c7b5ce196aae0bce066f44a
James Lunt <50912137+james-lunt@users.noreply.github.com>
2020-12-15 14:20:16 +0000

    Shows all repositories.
    Shows all my repositories instead of just one. Access token string changed to "Access Token" to say this is where you put your personal access token.

--- program.py
+++ program.py
@@ -1,8 +1,7 @@
 from github import Github
 
 #using access token
-g = Github("bef06aad3961363a56f1289c401e42d11e74e614")
+g = Github("Access Token")
 
 for repo in g.get_user().get_repos():
     print(repo.name)
-    repo.edit(has_wiki=False)
