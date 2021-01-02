24dd5c5a52638244f1778d0151f5bb1487713a16
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-01-02 17:21:56 +0000

    Optimised code and error handling.
    Github access functions optimised so users with a greater amounts of data can be accessed quicker.
    If an invalid user name is entered, the server goes to a page that will say this and allow person to try again.

--- mysite/metricvisualisation/views.py
+++ mysite/metricvisualisation/views.py
@@ -7,14 +7,14 @@ from django.http import HttpResponse
 #access token = personal access token from https://github.com/settings/tokens
 #using access token
 g = Github("")
-
+ 
 #get repos
 def getRepos(user):
         repo_list = []
         for repo in g.get_user(user).get_repos():
             repo_list.append(repo)
-        return repo_list
-
+        recent_repos = [repo_list[len(repo_list)-1], repo_list[len(repo_list)-2], repo_list[len(repo_list)-3], repo_list[len(repo_list)-4], repo_list[len(repo_list)-5] ]
+        return recent_repos
 
 #Get commits to a repo
 def getCommits(repo_list):
@@ -24,8 +24,8 @@ def getCommits(repo_list):
                 for commit in repo.get_commits():
                      commit_list.append(commit)
                 commit_amount_list.append(len(commit_list))
-        return commit_amount_list
-
+        return commit_amount_list     
+        
 
 #Get contributors to a repo
 def getContribs(repo_list):
@@ -39,14 +39,14 @@ def getContribs(repo_list):
 
 
 #Get pull requests to a repo
-def getPr(repo_list):
-        pr_amount_list = []
+def getBranches(repo_list):
+        branch_amount_list = []
         for repo in repo_list:
-                  pr_list = []
-                  for pr in repo.get_pulls():
-                         pr_list.append(pr)
-                  pr_amount_list.append(len(pr_list))
-        return pr_amount_list
+                  branch_list = []
+                  for branch in repo.get_branches():
+                         branch_list.append(branch)
+                  branch_amount_list.append(len(branch_list))
+        return branch_amount_list
 
 
 
@@ -56,73 +56,80 @@ def index(request):
     repo_list = getRepos("james-lunt")
     commit_amount_list = getCommits(repo_list)
     contributor_amount_list = getContribs(repo_list)
-    pr_amount_list = getPr(repo_list)
-   
-    #parse date to be visualise
-    repo1 = repo_list[len(repo_list)-1].name
-    repo2 = repo_list[len(repo_list)-2].name
-    repo3 = repo_list[len(repo_list)-3].name
-    repo4 = repo_list[len(repo_list)-4].name
-    repo5 = repo_list[len(repo_list)-5].name
+    branch_amount_list = getBranches(repo_list)
 
-    repoCommit1  = commit_amount_list[len(commit_amount_list)-1]
-    repoCommit2  = commit_amount_list[len(commit_amount_list)-2]
-    repoCommit3  = commit_amount_list[len(commit_amount_list)-3]
-    repoCommit4  = commit_amount_list[len(commit_amount_list)-4]
-    repoCommit5  = commit_amount_list[len(commit_amount_list)-5]
+    return render(
+        request,
+        'index.html',
+        {
+        'person':user,
+        
+        'repo1':repo_list[0].name,
+        'repo2':repo_list[1].name,
+        'repo3':repo_list[2].name,
+        'repo4':repo_list[3].name,
+        'repo5':repo_list[4].name,
 
-    repoContrib1 = contributor_amount_list[len(contributor_amount_list)-1]
-    repoContrib2 = contributor_amount_list[len(contributor_amount_list)-2]
-    repoContrib3 = contributor_amount_list[len(contributor_amount_list)-3]
-    repoContrib4 = contributor_amount_list[len(contributor_amount_list)-4]
-    repoContrib5 = contributor_amount_list[len(contributor_amount_list)-5]
+        'repoCommit1':commit_amount_list[0],
+        'repoCommit2':commit_amount_list[1],
+        'repoCommit3':commit_amount_list[2],
+        'repoCommit4':commit_amount_list[3],
+        'repoCommit5':commit_amount_list[4],
 
-    repoPr1 = pr_amount_list[len(pr_amount_list)-1]
-    repoPr2 = pr_amount_list[len(pr_amount_list)-2]
-    repoPr3 = pr_amount_list[len(pr_amount_list)-3]
-    repoPr4 = pr_amount_list[len(pr_amount_list)-4]
-    repoPr5 = pr_amount_list[len(pr_amount_list)-5]
+        'repoContrib1':contributor_amount_list[0],
+        'repoContrib2':contributor_amount_list[1],
+        'repoContrib3':contributor_amount_list[2],
+        'repoContrib4':contributor_amount_list[3],
+        'repoContrib5':contributor_amount_list[4],
 
-
-    return render(request, 'index.html', {'person' : user, 'repo1' : repo1, 'repo2' : repo2, 'repo3' : repo3, 'repo4' : repo4, 'repo5' : repo5,
-                                           'repoCommit1' : repoCommit1, 'repoCommit2' : repoCommit2, 'repoCommit3' : repoCommit3, 'repoCommit4' : repoCommit4, 'repoCommit5' : repoCommit5,
-                                           'repoContrib1' : repoContrib1, 'repoContrib2' : repoContrib2, 'repoContrib3' : repoContrib3, 'repoContrib4' : repoContrib4, 'repoContrib5' : repoContrib5,
-                                           'repoPr1' : repoPr1, 'repoPr2' : repoPr2, 'repoPr3' : repoPr3, 'repoPr4' : repoPr4, 'repoPr5' : repoPr5})
+        'repoBranch1':branch_amount_list[0],
+        'repoBranch2':branch_amount_list[1],
+        'repoBranch3':branch_amount_list[2],
+        'repoBranch4':branch_amount_list[3],
+        'repoBranch5':branch_amount_list[4]
+        }
+    )
 
 def find(request):
-        
+  
     user = request.GET['search']
-    repo_list = getRepos(user)
-    commit_amount_list = getCommits(repo_list)
-    contributor_amount_list = getContribs(repo_list)
-    pr_amount_list = getPr(repo_list)
-   
-    #parse date to be visualise
-    repo1 = repo_list[len(repo_list)-1].name
-    repo2 = repo_list[len(repo_list)-2].name
-    repo3 = repo_list[len(repo_list)-3].name
-    repo4 = repo_list[len(repo_list)-4].name
-    repo5 = repo_list[len(repo_list)-5].name
 
-    repoCommit1  = commit_amount_list[len(commit_amount_list)-1]
-    repoCommit2  = commit_amount_list[len(commit_amount_list)-2]
-    repoCommit3  = commit_amount_list[len(commit_amount_list)-3]
-    repoCommit4  = commit_amount_list[len(commit_amount_list)-4]
-    repoCommit5  = commit_amount_list[len(commit_amount_list)-5]
+    try:
+        repo_list = getRepos(user)
+        commit_amount_list = getCommits(repo_list)
+        contributor_amount_list = getContribs(repo_list)
+        branch_amount_list = getBranches(repo_list)
+        
+        return render(
+                request,
+                'index.html',
+                {
+                'person':user,
+                
+                'repo1':repo_list[0].name,
+                'repo2':repo_list[1].name,
+                'repo3':repo_list[2].name,
+                'repo4':repo_list[3].name,
+                'repo5':repo_list[4].name,
 
-    repoContrib1 = contributor_amount_list[len(contributor_amount_list)-1]
-    repoContrib2 = contributor_amount_list[len(contributor_amount_list)-2]
-    repoContrib3 = contributor_amount_list[len(contributor_amount_list)-3]
-    repoContrib4 = contributor_amount_list[len(contributor_amount_list)-4]
-    repoContrib5 = contributor_amount_list[len(contributor_amount_list)-5]
+                'repoCommit1':commit_amount_list[0],
+                'repoCommit2':commit_amount_list[1],
+                'repoCommit3':commit_amount_list[2],
+                'repoCommit4':commit_amount_list[3],
+                'repoCommit5':commit_amount_list[4],
 
-    repoPr1 = pr_amount_list[len(pr_amount_list)-1]
-    repoPr2 = pr_amount_list[len(pr_amount_list)-2]
-    repoPr3 = pr_amount_list[len(pr_amount_list)-3]
-    repoPr4 = pr_amount_list[len(pr_amount_list)-4]
-    repoPr5 = pr_amount_list[len(pr_amount_list)-5]
+                'repoContrib1':contributor_amount_list[0],
+                'repoContrib2':contributor_amount_list[1],
+                'repoContrib3':contributor_amount_list[2],
+                'repoContrib4':contributor_amount_list[3],
+                'repoContrib5':contributor_amount_list[4],
 
-    return render(request, 'index.html', {'person' : user, 'repo1' : repo1, 'repo2' : repo2, 'repo3' : repo3, 'repo4' : repo4, 'repo5' : repo5,
-                                           'repoCommit1' : repoCommit1, 'repoCommit2' : repoCommit2, 'repoCommit3' : repoCommit3, 'repoCommit4' : repoCommit4, 'repoCommit5' : repoCommit5,
-                                           'repoContrib1' : repoContrib1, 'repoContrib2' : repoContrib2, 'repoContrib3' : repoContrib3, 'repoContrib4' : repoContrib4, 'repoContrib5' : repoContrib5,
-                                           'repoPr1' : repoPr1, 'repoPr2' : repoPr2, 'repoPr3' : repoPr3, 'repoPr4' : repoPr4, 'repoPr5' : repoPr5})
+                'repoBranch1':branch_amount_list[0],
+                'repoBranch2':branch_amount_list[1],
+                'repoBranch3':branch_amount_list[2],
+                'repoBranch4':branch_amount_list[3],
+                'repoBranch5':branch_amount_list[4]
+                }
+        )
+    except:
+           return render(request,'find.html')
